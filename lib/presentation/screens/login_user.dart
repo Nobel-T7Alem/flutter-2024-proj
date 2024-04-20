69353df6e280e7356236f40f3c375169fadc5847
ruthalemfanta <133218569+ruthalemfanta@users.noreply.github.com>
2024-04-20 23:36:05 +0300

    Login page update

--- lib/presentation/screens/login_user.dart
+++ lib/presentation/screens/login_user.dart
@@ -12,114 +12,132 @@ class LoginUser extends StatefulWidget {
 class _LoginUserState extends State<LoginUser> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 16),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                child: Row(
-                  children: [
-                    IconButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        icon: const Icon(Icons.arrow_back)),
-                    Image.asset(
-                      'assets/images/sebawilogo.png',
-                      width: 120.0,
-                      height: 120.0,
-                    ),
-                    const Text(
-                      'Log In',
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 35,
-                          fontWeight: FontWeight.bold),
-                    )
-                  ],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 35),
+                  child: Image.asset(
+                    'assets/images/sebawilogo.png',
+                    width: 140.0,
+                    height: 140.0,
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 2.0,
-              ),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
+                Padding(
+                  padding: const EdgeInsets.only(left: 35),
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SizedBox(
+                  height: 2.0,
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 35.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 8.0),
+                      Text(
+                        'Welcome back.',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 35.0),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.only(bottom: 4.0),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Color.fromARGB(255, 31, 78, 33),
+                              style: BorderStyle.solid,
+                              width: 2)),
+                      enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Color.fromRGBO(213, 213, 213, 1))),
+                      labelText: 'Username',
+                      labelStyle: TextStyle(
+                          color: Color.fromARGB(255, 165, 165, 165),
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 35.0),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.only(bottom: 4.0),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Color.fromARGB(255, 31, 78, 33),
+                              style: BorderStyle.solid,
+                              width: 2)),
+                      enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Color.fromRGBO(213, 213, 213, 1))),
+                      labelText: 'Password',
+                      labelStyle: TextStyle(
+                          color: Color.fromARGB(255, 165, 165, 165),
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    obscureText: true,
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                SizedBox(height: 40.0),
+                CustomButton(
+                    buttonText: 'Login',
+                    buttonColor: Color.fromARGB(255, 83, 171, 71),
+                    buttonTextColor: Colors.white,
+                    buttonAction: () {
+                      Navigator.pushNamed(context, '/volunteer_page');
+                    }),
+                Padding(
+                  padding: EdgeInsets.all(17),
+                  child: Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Text(
-                          'Welcome back ,',
-                          style: TextStyle(
-                              fontSize: 30, fontWeight: FontWeight.bold),
+                        const Padding(
+                          padding: EdgeInsets.only(right: 4.0),
+                          child: Text('No account?'),
                         ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pushNamed(context, '/signup');
+                          },
+                          child: const Text(
+                            'Signup',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(255, 66, 148, 69),
+                            ),
+                          ),
+                        )
                       ],
                     ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 10.0),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                    contentPadding: EdgeInsets.only(bottom: 4.0),
-                    focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Color.fromARGB(255, 31, 78, 33),
-                            style: BorderStyle.solid,
-                            width: 2)),
-                    enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Color.fromRGBO(213, 213, 213, 1))),
-                    labelText: 'Username',
-                    labelStyle: TextStyle(
-                        color: Color.fromARGB(255, 165, 165, 165),
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600),
                   ),
-                  style: TextStyle(color: Colors.black),
-                ),
-              ),
-              SizedBox(height: 10.0),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                    contentPadding: EdgeInsets.only(bottom: 4.0),
-                    focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Color.fromARGB(255, 31, 78, 33),
-                            style: BorderStyle.solid,
-                            width: 2)),
-                    enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Color.fromRGBO(213, 213, 213, 1))),
-                    labelText: 'Password',
-                    labelStyle: TextStyle(
-                        color: Color.fromARGB(255, 165, 165, 165),
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600),
-                  ),
-                  obscureText: true,
-                  style: TextStyle(color: Colors.black),
-                ),
-              ),
-              SizedBox(height: 40.0),
-              CustomButton(
-                buttonText: 'Log In',
-                buttonColor: Color.fromARGB(255, 83, 171, 71),
-                buttonTextColor: Colors.white,
-                buttonAction: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => AdminLoginPage()),
-                  );
-                },
-              ),
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
